674739c74dd5ea77138e0251cccb3ef3d2520a64
Kevin Bulloch <kevinb93@hotmail.co.uk>
2020-10-23 09:47:48 +0100

    add token to signup route

--- backend/src/controllers/users-controllers.ts
+++ backend/src/controllers/users-controllers.ts
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import HttpError from '../models/http-error';
 import User from '../models/user';
 import { NextFunction, Request, Response } from 'express';
+import secret from 'utils/secret';
 
 export const signup = async (
     req: Request,
@@ -60,7 +61,22 @@ export const signup = async (
         );
     }
 
-    res.status(201).json({ userId: newUser.id, email: newUser.email });
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: newUser.id, email: newUser.email },
+            secret.jwtPrivateKey,
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError(
+            'Signing up failed, please try again later.',
+            500
+        );
+        return next(error);
+    }
+
+    res.status(201).json({ userId: newUser.id, email: newUser.email, token });
 };
 
 export const login = async (
@@ -108,7 +124,7 @@ export const login = async (
     try {
         token = jwt.sign(
             { userId: existingUser.id, email: existingUser.email },
-            'supersecret_dont_share',
+            secret.jwtPrivateKey,
             { expiresIn: '1h' }
         );
     } catch (err) {
